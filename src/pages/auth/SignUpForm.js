596ace20db8153932a250b8d3b7c7938d91ae8bf
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-02-04 22:58:19 +0100

    feat: add authentitication feature to the form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,19 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Image,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+
+  const { username, password1, password2 } = signUpData;
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      // Computed property name JavaScript feature:
+      // This line creates a key value  pair, with the key being the input fieldname,
+      // and the value being the value entered by the user.
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
               <Form.Label className="d-none">username</Form.Label>
               <Form.Control
@@ -21,8 +61,17 @@ const SignUpForm = () => {
                 type="text"
                 placeholder="username"
                 name="username"
+                value={username}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.username?.map((message, i) => {
+              return (
+                <Alert variant="warning" key={i}>
+                  {message}
+                </Alert>
+              );
+            })}
 
             <Form.Group controlId="password1">
               <Form.Label className="d-none">password</Form.Label>
@@ -31,8 +80,17 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="password"
                 name="password1"
+                value={password1}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.password1?.map((message, i) => {
+              return (
+                <Alert variant="warning" key={i}>
+                  {message}
+                </Alert>
+              );
+            })}
 
             <Form.Group controlId="password2">
               <Form.Label className="d-none">confirm password</Form.Label>
@@ -41,8 +99,17 @@ const SignUpForm = () => {
                 type="password"
                 placeholder="confirm password"
                 name="password2"
+                value={password2}
+                onChange={handleChange}
               />
             </Form.Group>
+            {errors.password2?.map((message, i) => {
+              return (
+                <Alert variant="warning" key={i}>
+                  {message}
+                </Alert>
+              );
+            })}
 
             <Button
               type="submit"
@@ -50,6 +117,13 @@ const SignUpForm = () => {
             >
               Sign up
             </Button>
+            {errors.non_field_errors?.map((message, i) => {
+              return (
+                <Alert variant="warning" key={i} className="mt-3">
+                  {message}
+                </Alert>
+              );
+            })}
           </Form>
         </Container>
 
