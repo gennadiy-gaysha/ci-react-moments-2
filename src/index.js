115acc3e0cb08ca71be74e5fe9ecd6394c92b871
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-02-04 19:24:45 +0100

    feat: add routing to navbar links

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // This snippet was automatically created when running npx create-react-app .
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +15,9 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
